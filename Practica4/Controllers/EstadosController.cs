839ffc8d48109e412d283df911bd573d3a8914f8
Navidad <chocobanano2000slv@gmail.com>
2025-03-20 17:43:53 -0600

    parte 2

--- Practica4/Controllers/EstadosController.cs
+++ Practica4/Controllers/EstadosController.cs
@@ -31,9 +31,109 @@ namespace Practica4.Controllers
 
                 _context.Estados.Add(estado);
                 _context.SaveChanges();
+
+                //Mensaje de exito
+                TempData["Mensaje"] = "El estado se ha creado corretamente";
+                TempData["MessageType"] = "success";
+
                 return RedirectToAction("Index");
             }
+
+            //Mensaje de error
+            TempData["Message"] = "Erro al crear el estado. Verifica los datos";
+            TempData["MessageType"] = "error";
+
             return View(estado);
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)           
+            {
+                return NotFound();
+            }
+            var estado = _context.Estados.Find(id);
+
+            if (estado == null)
+            {
+                return NotFound();
+            }
+            return View(estado);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var estado = _context.Estados.Find(id);
+
+            if (estado == null)
+            {
+                return NotFound();
+            }
+            return View(estado);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Edit(int id, Estado estado)
+        {
+            if (id != estado.Id)
+            {
+                return NotFound();
+            }
+
+            var current = _context.Estados.Find(id);
+
+            if (current == null)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                current.Name = estado.Name;
+                current.Descripcion = estado.Descripcion;
+                current.Color = estado.Color;
+                current.UpdatedAt = DateTime.Now;
+
+                _context.Update(current);
+                _context.SaveChanges();
+
+                //Mensaje de exito
+                TempData["Message"] = "El estado se ha actualizado correctamente";
+                TempData["MessageType"] = "success";
+                return RedirectToAction("Index");
+            }
+
+            //Mensaje de error
+            TempData["Message"] = "Error al actualizar el estado. Verifica los datos";
+            TempData["MessageType"] = "error";
+            return View(estado);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Delete(int id)
+        {
+            var estado = _context.Estados.Find(id);
+
+            if (estado == null)
+            {
+                return NotFound();
+            }
+
+            _context.Estados.Remove(estado);
+            _context.SaveChanges();
+
+            //Mensaje de exito
+            TempData["Message"] = "El estado se ha eliminado correctamente";
+            TempData["MessageType"] = "success";
+            return RedirectToAction("Index");
         }
     }
 }
